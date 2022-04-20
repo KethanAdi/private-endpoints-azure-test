5e0c154f8b455915bb3eb86847e3d2ceb18f1e5c
KethanAdi <102666849+KethanAdi@users.noreply.github.com>
2022-04-21 01:23:29 +0530

    Update main.tf

--- eventhub/public-endpoint/main.tf
+++ eventhub/public-endpoint/main.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.resource_group_location
 }
 
-resource "azurerm_eventhub_namespace" "example" {
+resource "azurerm_eventhub_namespace" "event_namespace" {
   name                = "TestEventHubNamespace"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   sku                 = "Standard"
   capacity            = 1
 
@@ -15,10 +15,10 @@ resource "azurerm_eventhub_namespace" "example" {
   }
 }
 
-resource "azurerm_eventhub" "example" {
+resource "azurerm_eventhub" "eventhub1" {
   name                = "TestEventHub"
-  namespace_name      = azurerm_eventhub_namespace.example.name
-  resource_group_name = azurerm_resource_group.example.name
+  namespace_name      = azurerm_eventhub_namespace.event_namespace.name
+  resource_group_name = azurerm_resource_group.rg.name
   partition_count     = 2
   message_retention   = 1
 }
