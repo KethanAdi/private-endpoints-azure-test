2a32eacf0930d71400f20851fa47d4dd6c3cc1b7
KethanAdi <102666849+KethanAdi@users.noreply.github.com>
2022-04-21 18:12:12 +0530

    Update app-everything.tf

--- sqlserver/app-everything.tf
+++ sqlserver/app-everything.tf
@@ -27,7 +27,7 @@ provider "azurerm" {
 ####################
 
 # Create a resource group
-#resource "azurerm_resource_group" "kopi-rg" {
+#resource "data.azurerm_resource_group" "ktest-rg" {
 #  name     = var.resource_group_name
 #  location = var.location
 #
@@ -37,11 +37,11 @@ provider "azurerm" {
 #}
 
 # Create the VNET
-resource "azurerm_virtual_network" "kopi-vnet" {
+resource "azurerm_virtual_network" "ktest-vnet" {
   name                = "${var.prefix}-${var.environment}-${var.app_name}-vnet"
-  address_space       = [var.kopi-vnet-cidr]
-  location            = azurerm_resource_group.kopi-rg.location
-  resource_group_name = azurerm_resource_group.kopi-rg.name
+  address_space       = [var.ktest-vnet-cidr]
+  location            = data.azurerm_resource_group.ktest-rg.location
+  resource_group_name = data.azurerm_resource_group.ktest-rg.name
 
   tags = {
     environment = var.environment
@@ -49,11 +49,11 @@ resource "azurerm_virtual_network" "kopi-vnet" {
 }
 
 # Create a DB subnet
-resource "azurerm_subnet" "kopi-db-subnet" {
+resource "azurerm_subnet" "ktest-db-subnet" {
   name                 = "${var.prefix}-${var.environment}-${var.app_name}-db-subnet"
-  address_prefixes     = [var.kopi-db-subnet-cidr]
-  virtual_network_name = azurerm_virtual_network.kopi-vnet.name
-  resource_group_name  = azurerm_resource_group.kopi-rg.name
+  address_prefixes     = [var.ktest-db-subnet-cidr]
+  virtual_network_name = azurerm_virtual_network.ktest-vnet.name
+  resource_group_name  = data.azurerm_resource_group.ktest-rg.name
   enforce_private_link_endpoint_network_policies = true
 }
 
@@ -63,23 +63,23 @@ resource "azurerm_subnet" "kopi-db-subnet" {
 ###################
 
 # Create a Private DNS Zone
-resource "azurerm_private_dns_zone" "kopi-private-dns" {
-  name                = var.kopi-private-dns
-  resource_group_name = azurerm_resource_group.kopi-rg.name
+resource "azurerm_private_dns_zone" "ktest-private-dns" {
+  name                = var.ktest-private-dns
+  resource_group_name = data.azurerm_resource_group.ktest-rg.name
 }
 
 # Link the Private DNS Zone with the VNET
-resource "azurerm_private_dns_zone_virtual_network_link" "kopi-private-dns-link" {
+resource "azurerm_private_dns_zone_virtual_network_link" "ktest-private-dns-link" {
   name                  = "${var.prefix}-${var.environment}-${var.app_name}-vnet"
-  resource_group_name   = azurerm_resource_group.kopi-rg.name
-  private_dns_zone_name = azurerm_private_dns_zone.kopi-private-dns.name
-  virtual_network_id    = azurerm_virtual_network.kopi-vnet.id
+  resource_group_name   = data.azurerm_resource_group.ktest-rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.ktest-private-dns.name
+  virtual_network_id    = azurerm_virtual_network.ktest-vnet.id
 }
 
 # Create a DB Private DNS Zone
-resource "azurerm_private_dns_zone" "kopi-endpoint-dns-private-zone" {
-  name                = "${var.kopi-dns-privatelink}.database.windows.net"
-  resource_group_name = azurerm_resource_group.kopi-rg.name
+resource "azurerm_private_dns_zone" "ktest-endpoint-dns-private-zone" {
+  name                = "${var.ktest-dns-privatelink}.database.windows.net"
+  resource_group_name = data.azurerm_resource_group.ktest-rg.name
 }
 
 ########################
@@ -87,47 +87,47 @@ resource "azurerm_private_dns_zone" "kopi-endpoint-dns-private-zone" {
 ########################
 
 # Create a DB Private Endpoint
-resource "azurerm_private_endpoint" "kopi-db-endpoint" {
-  depends_on = [azurerm_mssql_server.kopi-sql-server]
+resource "azurerm_private_endpoint" "ktest-db-endpoint" {
+  depends_on = [azurerm_mssql_server.ktest-sql-server]
 
   name                = "${var.prefix}-${var.environment}-${var.app_name}-db-endpoint"
-  location            = azurerm_resource_group.kopi-rg.location
-  resource_group_name = azurerm_resource_group.kopi-rg.name
-  subnet_id           = azurerm_subnet.kopi-db-subnet.id
+  location            = data.azurerm_resource_group.ktest-rg.location
+  resource_group_name = data.azurerm_resource_group.ktest-rg.name
+  subnet_id           = azurerm_subnet.ktest-db-subnet.id
 
   private_service_connection {
     name                           = "${var.prefix}-${var.environment}-${var.app_name}-db-endpoint"
     is_manual_connection           = "false"
-    private_connection_resource_id = azurerm_mssql_server.kopi-sql-server.id
+    private_connection_resource_id = azurerm_mssql_server.ktest-sql-server.id
     subresource_names              = ["sqlServer"]
   }
 }
 
 # DB Private Endpoint Connecton
-data "azurerm_private_endpoint_connection" "kopi-endpoint-connection" {
-  depends_on = [azurerm_private_endpoint.kopi-db-endpoint]
+data "azurerm_private_endpoint_connection" "ktest-endpoint-connection" {
+  depends_on = [azurerm_private_endpoint.ktest-db-endpoint]
 
-  name                = azurerm_private_endpoint.kopi-db-endpoint.name
-  resource_group_name = azurerm_resource_group.kopi-rg.name
+  name                = azurerm_private_endpoint.ktest-db-endpoint.name
+  resource_group_name = data.azurerm_resource_group.ktest-rg.name
 }
 
 # Create a DB Private DNS A Record
-resource "azurerm_private_dns_a_record" "kopi-endpoint-dns-a-record" {
-  depends_on = [azurerm_mssql_server.kopi-sql-server]
+resource "azurerm_private_dns_a_record" "ktest-endpoint-dns-a-record" {
+  depends_on = [azurerm_mssql_server.ktest-sql-server]
 
-  name                = lower(azurerm_mssql_server.kopi-sql-server.name)
-  zone_name           = azurerm_private_dns_zone.kopi-endpoint-dns-private-zone.name
-  resource_group_name = azurerm_resource_group.kopi-rg.name
+  name                = lower(azurerm_mssql_server.ktest-sql-server.name)
+  zone_name           = azurerm_private_dns_zone.ktest-endpoint-dns-private-zone.name
+  resource_group_name = data.azurerm_resource_group.ktest-rg.name
   ttl                 = 300
-  records             = [data.azurerm_private_endpoint_connection.kopi-endpoint-connection.private_service_connection.0.private_ip_address]
+  records             = [data.azurerm_private_endpoint_connection.ktest-endpoint-connection.private_service_connection.0.private_ip_address]
 }
 
 # Create a Private DNS to VNET link
 resource "azurerm_private_dns_zone_virtual_network_link" "dns-zone-to-vnet-link" {
   name                  = "${var.prefix}-${var.environment}-${var.app_name}-db-vnet-link"
-  resource_group_name   = azurerm_resource_group.kopi-rg.name
-  private_dns_zone_name = azurerm_private_dns_zone.kopi-endpoint-dns-private-zone.name
-  virtual_network_id    = azurerm_virtual_network.kopi-vnet.id
+  resource_group_name   = data.azurerm_resource_group.ktest-rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.ktest-endpoint-dns-private-zone.name
+  virtual_network_id    = azurerm_virtual_network.ktest-vnet.id
 }
 
 
@@ -136,13 +136,13 @@ resource "azurerm_private_dns_zone_virtual_network_link" "dns-zone-to-vnet-link"
 #######################
 
 # Create the SQL Server 
-resource "azurerm_mssql_server" "kopi-sql-server" {
+resource "azurerm_mssql_server" "ktest-sql-server" {
   name                          = "${var.prefix}-${var.environment}-${var.app_name}-sql-server" # NOTE: needs to be globally unique
-  resource_group_name           = azurerm_resource_group.kopi-rg.name
-  location                      = azurerm_resource_group.kopi-rg.location
+  resource_group_name           = data.azurerm_resource_group.ktest-rg.name
+  location                      = data.azurerm_resource_group.ktest-rg.location
   version                       = "12.0"
-  administrator_login           = var.kopi-sql-admin-username
-  administrator_login_password  = var.kopi-sql-admin-password
+  administrator_login           = var.ktest-sql-admin-username
+  administrator_login_password  = var.ktest-sql-admin-password
   public_network_access_enabled = false
 
   tags = {
@@ -151,13 +151,13 @@ resource "azurerm_mssql_server" "kopi-sql-server" {
 }
 
 # Create a the SQL database 
-resource "azurerm_sql_database" "kopi-sql-db" {
-  depends_on = [azurerm_mssql_server.kopi-sql-server]
+resource "azurerm_sql_database" "ktest-sql-db" {
+  depends_on = [azurerm_mssql_server.ktest-sql-server]
 
-  name                = "kopi-db"
-  resource_group_name = azurerm_resource_group.kopi-rg.name
-  location            = azurerm_resource_group.kopi-rg.location
-  server_name         = azurerm_mssql_server.kopi-sql-server.name
+  name                = "ktest-db"
+  resource_group_name = data.azurerm_resource_group.ktest-rg.name
+  location            = data.azurerm_resource_group.ktest-rg.location
+  server_name         = azurerm_mssql_server.ktest-sql-server.name
 
   tags = {
     environment = var.environment
