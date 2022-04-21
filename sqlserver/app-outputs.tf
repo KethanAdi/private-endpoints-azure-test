c53f3814b476adde59b5ccbbdff4f0d53d448900
KethanAdi <102666849+KethanAdi@users.noreply.github.com>
2022-04-21 18:13:22 +0530

    Update app-outputs.tf

--- sqlserver/app-outputs.tf
+++ sqlserver/app-outputs.tf
@@ -4,7 +4,7 @@
 
 output "sql_private_link_endpoint_ip" {
   description = "SQL Private Link Endpoint IP"
-  value = data.azurerm_private_endpoint_connection.kopi-endpoint-connection.private_service_connection.0.private_ip_address
+  value = data.azurerm_private_endpoint_connection.ktest-endpoint-connection.private_service_connection.0.private_ip_address
 }
 #########################
 ## SQL Server - Output ##
@@ -12,5 +12,5 @@ output "sql_private_link_endpoint_ip" {
 
 output "sql_db" {
   description = "SQL Server DB and Database"
-  value       = "${azurerm_sql_database.kopi-sql-db.server_name}/${azurerm_sql_database.kopi-sql-db.name}"
+  value       = "${azurerm_sql_database.ktest-sql-db.server_name}/${azurerm_sql_database.ktest-sql-db.name}"
 }
