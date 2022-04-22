ab5276c3b2aa1d0ad914e164bda45e86c47740e1
KethanAdi <102666849+KethanAdi@users.noreply.github.com>
2022-04-22 16:37:13 +0530

    Update app-outputs.tf

--- multiple/app-outputs.tf
+++ multiple/app-outputs.tf
@@ -4,7 +4,7 @@
 
 output "sql_private_link_endpoint_ip" {
   description = "SQL Private Link Endpoint IP"
-  value = data.azurerm_private_endpoint_connection.ktest-endpoint-connection.private_service_connection.0.private_ip_address
+  value = data.azurerm_private_endpoint_connection.ktest-db-endpoint-connection.private_service_connection.0.private_ip_address
 }
 #########################
 ## SQL Server - Output ##
