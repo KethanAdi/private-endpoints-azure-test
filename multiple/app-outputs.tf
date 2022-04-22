57e151caf5a32e6113609b26b76855e2ee10144a
KethanAdi <102666849+KethanAdi@users.noreply.github.com>
2022-04-22 17:09:34 +0530

    Update app-outputs.tf

--- multiple/app-outputs.tf
+++ multiple/app-outputs.tf
@@ -14,3 +14,24 @@ output "sql_db" {
   description = "SQL Server DB and Database"
   value       = "${azurerm_sql_database.ktest-sql-db.server_name}/${azurerm_sql_database.ktest-sql-db.name}"
 }
+
+
+
+output "Event_Hub_Namespace" {
+  description = "Event Hub Namespace"
+  value       = "${azurerm_eventhub_namespace.event_namespace1.name}"
+}
+
+
+output "Event_Hub" {
+  description = "Event Hub"
+  value       = "${azurerm_eventhub.event_hub1.name}"
+}
+
+
+
+output "Event_hub_private_link" {
+  description = "PRIVATELINK EVENTHUBNAMESPACE"
+  value = data.azurerm_private_endpoint_connection.ktest-event-endpoint-connection.private_service_connection.0.private_ip_address
+}
+
