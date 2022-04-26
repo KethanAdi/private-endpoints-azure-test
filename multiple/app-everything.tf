1a3e14e3646b7dde96f6ab8ab3e1b6bb93aa87fb
KethanAdi <102666849+KethanAdi@users.noreply.github.com>
2022-04-27 01:23:21 +0530

    Update app-everything.tf

--- multiple/app-everything.tf
+++ multiple/app-everything.tf
@@ -222,9 +222,9 @@ resource "azurerm_eventhub_namespace" "event_namespace1" {
   resource_group_name =data.azurerm_resource_group.ktest-rg.name
   sku                 = "Standard"
   capacity            = 1
-  network_rulesets    {
+  network_rulesets  = {
     default_action       = "Allow"
-    virtual_network_rule  {
+    virtual_network_rule = {
         subnet_id = azurerm_subnet.ktest-subnet.id
         ignore_missing_virtual_network_service_endpoint = "false"
     }
