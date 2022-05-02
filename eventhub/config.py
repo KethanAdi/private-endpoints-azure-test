14ae072320e787e70f3b89a9d0bec23ed9e9ebe2
KethanAdi <102666849+KethanAdi@users.noreply.github.com>
2022-05-02 18:40:34 +0530

    Update config.py

--- eventhub/config.py
+++ eventhub/config.py
@@ -1,4 +1,4 @@
 storage_endpoint="DefaultEndpointsProtocol=https;AccountName=s653829458i;AccountKey=KQOr/41tYR56HEXuwXyR+OB2NyYhMtTXdXuPUAENON/PM7MLxhAdpU3taen83ic4YFeB79jyn9e+0WKHjiuTzw==;EndpointSuffix=core.windows.net"
-eventhub_endpoint="Endpoint=sb://event12843496ssssss.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=clHqVN3h0gox7553soxYH8BXgV8CU7TWaxQHvIrsnkE="
+eventhubns_endpoint="Endpoint=sb://event12843496ssssss.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=clHqVN3h0gox7553soxYH8BXgV8CU7TWaxQHvIrsnkE="
 eventhub_namespace=""
 eventhub_name="event1"
