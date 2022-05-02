c34d08ab0b72a03c4994ea355ff2e4500a4b09b2
KethanAdi <102666849+KethanAdi@users.noreply.github.com>
2022-05-02 16:03:06 +0530

    Update receiver.py

--- eventhub/receiver.py
+++ eventhub/receiver.py
@@ -16,7 +16,7 @@ async def main():
     checkpoint_store = BlobCheckpointStore.from_connection_string("DefaultEndpointsProtocol=https;AccountName=tttttt762383232;AccountKey=msPbPTnBkjvoP3iFxH7EaVCdITuwRW8q78U82Yz1uS3Fv0l/OBu60gFve2oOnIr00K3O6iNEeZqV+7tNi+1AeA==;EndpointSuffix=core.windows.net", "container1")
 
     # Create a consumer client for the event hub.
-    client = EventHubConsumerClient.from_connection_string("Endpoint=sb://namespace16728537411.privatelink.servicebus.windows.net/;SharedAccessKeyName=c1;SharedAccessKey=OKOxRBceRPwTWvgUj5WxfwPsQrBQ8ZpLZpPlT0/3O0E=;EntityPath=eventhub1", consumer_group="$Default", eventhub_name="eventhub1", checkpoint_store=checkpoint_store)
+    client = EventHubConsumerClient.from_connection_string("Endpoint=sb://namespace16728537411.servicebus.windows.net/;SharedAccessKeyName=c1;SharedAccessKey=OKOxRBceRPwTWvgUj5WxfwPsQrBQ8ZpLZpPlT0/3O0E=;EntityPath=eventhub1", consumer_group="$Default", eventhub_name="eventhub1", checkpoint_store=checkpoint_store)
     async with client:
         # Call the receive method. Read from the beginning of the partition (starting_position: "-1")
         await client.receive(on_event=on_event,  starting_position="-1")
