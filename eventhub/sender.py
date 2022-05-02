8914779c667e60e95a3c295972317908e9d604ce
KethanAdi <102666849+KethanAdi@users.noreply.github.com>
2022-05-02 16:16:25 +0530

    Update sender.py

--- eventhub/sender.py
+++ eventhub/sender.py
@@ -6,7 +6,7 @@ async def run():
     # Create a producer client to send messages to the event hub.
     # Specify a connection string to your event hubs namespace and
     # the event hub name.
-    producer = EventHubProducerClient.from_connection_string(conn_str="Endpoint=sb://namespace16728537411ss.servicebus.windows.net/;SharedAccessKeyName=s1;SharedAccessKey=60OL4kXaIEj3oY8hAL2LoXdtIVHGzyQMpScx23Um614=;EntityPath=eventhub1", eventhub_name="eventhub1")
+    producer = EventHubProducerClient.from_connection_string(conn_str="Endpoint=sb://namespace16728537411ss.servicebus.windows.net/;SharedAccessKeyName=ss;SharedAccessKey=KEK7vC6el8slLNSuZHMoXI2pUTA2BK0utkokLNdzjvk=", eventhub_name="eventhub1")
     async with producer:
         # Create a batch.
         event_data_batch = await producer.create_batch()
