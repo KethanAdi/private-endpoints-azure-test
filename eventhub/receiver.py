26918886315148750948c570d0563183f3912b12
KethanAdi <102666849+KethanAdi@users.noreply.github.com>
2022-05-02 15:47:46 +0530

    Update receiver.py

--- eventhub/receiver.py
+++ eventhub/receiver.py
@@ -13,10 +13,10 @@ async def on_event(partition_context, event):
 
 async def main():
     # Create an Azure blob checkpoint store to store the checkpoints.
-    checkpoint_store = BlobCheckpointStore.from_connection_string("DefaultEndpointsProtocol=https;AccountName=s87947s879878907;AccountKey=muo3lk0YgRJOdEhfchhMw2HVaotPkVeLsUfQouyv7vcPEwW+xAQmnAFrFknRxuFswZzE9+Hk+grnqFKXj71ufg==;EndpointSuffix=core.windows.net", "container1")
+    checkpoint_store = BlobCheckpointStore.from_connection_string("DefaultEndpointsProtocol=https;AccountName=tttttt762383232;AccountKey=msPbPTnBkjvoP3iFxH7EaVCdITuwRW8q78U82Yz1uS3Fv0l/OBu60gFve2oOnIr00K3O6iNEeZqV+7tNi+1AeA==;EndpointSuffix=core.windows.net", "container1")
 
     # Create a consumer client for the event hub.
-    client = EventHubConsumerClient.from_connection_string("Endpoint=sb://exampletest1ns23.servicebus.windows.net/;SharedAccessKeyName=tr1;SharedAccessKey=MVtjK5zr7WE7nzgTD3/uvO5o5Oev8jOXsZvoRKYFnwg=;EntityPath=eventhub1", consumer_group="$Default", eventhub_name="eventhub1", checkpoint_store=checkpoint_store)
+    client = EventHubConsumerClient.from_connection_string("Endpoint=sb://namespace16728537411.servicebus.windows.net/;SharedAccessKeyName=c1;SharedAccessKey=OKOxRBceRPwTWvgUj5WxfwPsQrBQ8ZpLZpPlT0/3O0E=;EntityPath=eventhub1", consumer_group="$Default", eventhub_name="eventhub1", checkpoint_store=checkpoint_store)
     async with client:
         # Call the receive method. Read from the beginning of the partition (starting_position: "-1")
         await client.receive(on_event=on_event,  starting_position="-1")
